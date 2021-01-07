918d31901715651c233a92c7c8acb2819d608499
FriendA97 <frienda97.it@gmail.com>
2021-01-08 00:37:08 +0200

    Added front-end build folder, static middleware to render it

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(express.static("build"));
 morgan.token("postBody", (req, res) => {
   if (req.method === "POST") {
     return JSON.stringify(req.body);
